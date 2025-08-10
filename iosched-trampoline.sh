7b646b26c67a6f1568f433ce219c8f05e24a916d
AuxXxilium <info@auxxxilium.tech>
2025-08-10 16:07:04 +0200

    ioshed: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- iosched-trampoline.sh
+++ iosched-trampoline.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env sh
-
+#!/bin/sh
 # This script is saved to /sbin/modprobe which is a so called UMH (user-mode-helper) for kmod (kernel/kmod.c)
 # The kmod subsystem in the kernel is used to load modules from kernel. We exploit it a bit to load RP as soon as
 # possible (which turns out to be via init/main.c => load_default_modules => load_default_elevator_module
